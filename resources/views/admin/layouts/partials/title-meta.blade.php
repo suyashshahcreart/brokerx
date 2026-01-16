1e9c4ee861fe86712c48fad338cd3ddd3a4a1fe9
DeepeshSuryawanshi-CreArt <deepesh@creartsolutions.com>
2026-01-16 17:22:19 +0530

    no index no follow add in meta title.

--- resources/views/admin/layouts/partials/title-meta.blade.php
+++ resources/views/admin/layouts/partials/title-meta.blade.php
@@ -4,6 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="author" content="PROP PIK" />
 <meta name="robots" content="noindex, nofollow">
+
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
